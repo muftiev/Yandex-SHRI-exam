04531b29e6c2ca4c7db32f37ab5e921029ba6686
muftiev.rr <muftiev.rr@yandex.ua>
2013-07-22 23:15:05 +0300

    slider init

--- shri.js
+++ shri.js
@@ -6,6 +6,9 @@ jQuery(document).ready(function($){
 
 jQuery.fn.yandex_galery = function() {
 
-    
+    var target = $(this);
+    var galeryblock = '<div class="galery-wrapper"><ul class="list-photo-XXS"></ul>';
+    var slideblock = '<div class="slide-wrapper"><div class="large-img-wrapper"><div class="large-img-view"></div><button class="nav nav-left hidden"></button><button class="nav nav-right hidden"></button></div></div>';
+    $(target).append(slideblock).append(galeryblock);
 
 };
