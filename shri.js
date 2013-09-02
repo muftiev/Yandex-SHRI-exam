ecb0a8cc72dbc2a95c59e50eacfd9a2c8bb905ec
muftiev.rr <muftiev.rr@yandex.ua>
2013-09-02 22:17:41 +0300

    DOM generation and vars optimisation

--- shri.js
+++ shri.js
@@ -6,21 +6,51 @@ jQuery(document).ready(function($){
 
 jQuery.fn.yandex_galery = function() {
 
-    var target = $(this);
-    var galeryblock = '<div class="galery-wrapper"><ul class="list-photo-XXS"></ul></div><div id="loader"><img src="images/load.gif" alt="loading" /></div>';
-    var slideblock = '<div class="slide-wrapper"><div class="large-img-wrapper"><div class="large-img-view"></div><button class="nav nav-left hidden"></button><button class="nav nav-right hidden"></button></div></div>';
-    $(target).append(slideblock).append(galeryblock);
+    var target = $(this),
+        galerywrapper = $("<div/>")
+            .addClass("galery-wrapper")
+            .appendTo(target),
+        listphotoXXS = $("<ul/>")
+            .addClass("list-photo-XXS")
+            .appendTo(galerywrapper),
+        loader = $("<div/>")
+            .attr("id", "loader")
+            .appendTo(target),
+        slidewrapper = $("<div/>")
+            .addClass("slide-wrapper")
+            .prependTo(target),
+        largeimgwrapper = $("<div/>")
+            .addClass("large-img-wrapper")
+            .appendTo(slidewrapper);
 
-    var url = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json&limit=30";
-    var upd = checkURL();    
+    $("<div/>")
+        .addClass("large-img-view")
+        .appendTo(largeimgwrapper);
+    $("<button/>")
+        .addClass("nav nav-left hidden")
+        .appendTo(largeimgwrapper);
+    $("<button/>")
+        .addClass("nav nav-right hidden")
+        .appendTo(largeimgwrapper);
+    $("<img/>")
+        .attr("src", "images/load.gif")
+        .attr("alt", "loading")
+        .appendTo(loader);
+
+    var url = "http://api-fotki.yandex.ru/api/users/aig1001/album/63684/photos/?format=json&limit=30",
+        upd = checkURL();
+
     getPhotos(url, true);
 
-    $(".list-photo-XXS").mousewheel(function(event, delta) {
-        var position = parseInt($(this).css("left"));
-        var width = parseInt($(this).outerWidth()) - parseInt($(this).parent().outerWidth());
-        var slide = 500;
-        if(position+slide*delta>0) slide = 0-position;
-        if(position+slide*delta<-width) slide = width+position;
+    listphotoXXS.mousewheel(function(event, delta) {
+        var position = parseInt($(this).css("left")),
+            width = parseInt($(this).outerWidth()) - parseInt($(this).parent().outerWidth()),
+            slide = 500;
+
+        //if(position+slide*delta>0) slide = 0-position;
+        slide = (position+slide*delta>0)? 0-position : slide;
+        //if(position+slide*delta<-width) slide = width+position;
+        slide = (position+slide*delta<-width)? width+position : slide;
         if((position+slide*delta*2<-width) && (position+slide*delta>-width)) {
             var next = $(this).children().last().find("img").attr("data-next");
             if(next!=="undefined") getPhotos(next, false);
@@ -29,9 +59,10 @@ jQuery.fn.yandex_galery = function() {
     }); 
 
     $(document).on("click", ".list-item", function(){
-        var direction = scrollgallery(this);
-        var url = $(this).find("img").attr("data-l-src");
-        var alt = $(this).find("img").attr("alt");
+        var direction = scrollgallery(this),
+            url = $(this).find("img").attr("data-l-src"),
+            alt = $(this).find("img").attr("alt");
+
         togglenavigation(true);
         showPhoto(url, alt, direction);        
     });
@@ -63,20 +94,44 @@ function getPhotos(url, first){
             $("#loader").show();
         },
         success: function(data){
-            var limit = data.entries.length;
+            var limit = data.entries.length,
+                listphotoXXS = $(".list-photo-XXS"),
+                listitem = listphotoXXS.find(".list-item"),
+                li_width = listitem.outerWidth()+parseInt(listitem.css("margin-left"))+parseInt(listitem.css("margin-right")),
+                ul_width = (listitem.size() + limit) * li_width;
+
             for(var i=0; i<limit; i++){ 
-                var data_next = (i==limit-1 ? 'data-next="'+data.links.next+'"' : '');
-                $(".list-photo-XXS").append('<li class="list-item"><a href="#?imgId='+data.entries[i].updated+'" class="list-item-link"><img class="list-photo-XXS-item-img" data-upd="'+data.entries[i].updated+'" data-L-src="'+data.entries[i].img.L.href+'" '+data_next+' src="'+data.entries[i].img.XXS.href+'" alt="'+data.entries[i].title+'" /></li>');
+                var listitemlink = $("<a/>")
+                        .addClass("list-item-link")
+                        .attr("href", "#?imgId="+data.entries[i].updated),
+                    listphotoXXSitemimg = $("<img/>")
+                        .addClass("list-photo-XXS-item-img")
+                        .attr("data-upd", data.entries[i].updated)
+                        .attr("data-L-src", data.entries[i].img.L.href)
+                        .attr("src", data.entries[i].img.XXS.href)
+                        .attr("alt", data.entries[i].title)
+                        //.attr("data-next", data_next)
+                        .appendTo(listitemlink);
+
+                if(i==limit-1) listphotoXXSitemimg.attr("data-next", data.links.next);
+                $("<li/>")
+                    .addClass("list-item")
+                    .append(listitemlink)
+                    .appendTo(listphotoXXS);
             }
-            var li_width = $(".list-item").outerWidth()+parseInt($(".list-item").css("margin-left"))+parseInt($(".list-item").css("margin-right"));
-            var ul_width = $(".list-photo-XXS li").size() * li_width;
-            $(".list-photo-XXS").css("width", ul_width);
+
+            var listitem = listphotoXXS.find(".list-item"),
+                li_width = listitem.outerWidth()+parseInt(listitem.css("margin-left"))+parseInt(listitem.css("margin-right")),
+                ul_width = listitem.size() * li_width;
+
+            listphotoXXS.css("width", ul_width);
         },
         complete: function(){
             $("#loader").hide();
             if(first) {
-                var upd = checkURL();
-                var curphoto = (upd.length>0) ? $('[data-upd="'+upd+'"]').parents().eq(1) : $(".list-photo-XXS .list-item:first-child");
+                var upd = checkURL(),
+                    curphoto = (upd.length>0) ? $('[data-upd="'+upd+'"]').parents().eq(1) : $(".list-photo-XXS .list-item:first-child");
+
                 curphoto.click();
             }
         }
@@ -84,71 +139,97 @@ function getPhotos(url, first){
 }
 
 function showPhoto(url, alt, direction) {
-    var target = $(".active-img");
-    if(!direction) {
-        $(".large-img-view").html('<img class="slide-img active-img" src="'+url+'" alt="'+alt+'" />');
+    var target = $(".active-img"),
+        largeimgview = $(".large-img-view");
+
+    if(!direction) {        
+        var slideimg = $("<img/>")
+            .addClass("slide-img active-img")
+            .attr("src", url)
+            .attr("alt", alt);
+
+        largeimgview.html(slideimg);
     } else if(direction>0) {
-        $(".large-img-view").append('<img class="slide-img next-img" src="'+url+'" alt="'+alt+'" />');
+        //largeimgview.append('<img class="slide-img next-img" src="'+url+'" alt="'+alt+'" />');
+        $("<img/>")
+            .addClass("slide-img next-img")
+            .attr("src", url)
+            .attr("alt", alt)
+            .appendTo(largeimgview);
+
         $(".next-img").on("load", function() {
             target.animate({"left" : -500}, 100, function(){
-                target.removeClass("active-img");
-                target.addClass("prev-img");
-                target.remove();
+                target.removeClass("active-img")
+                    .addClass("prev-img")
+                    .remove();
             });    
-            var current = $(".next-img");
-            var slide = (window.outerWidth-current.outerWidth())/2;
+            var current = $(".next-img"),
+                slide = (window.outerWidth-current.outerWidth())/2;
+
             current.animate({"right" : slide}, 100, function(){
-                current.removeClass("next-img");
-                current.css("right", "");
-                current.addClass("active-img");
+                current.removeClass("next-img")
+                    .css("right", "")
+                    .addClass("active-img");
                 target.remove();
             });            
         });        
     } else {
-        $(".large-img-view").prepend('<img class="slide-img prev-img" src="'+url+'" alt="'+alt+'" />');
+        //largeimgview.prepend('<img class="slide-img prev-img" src="'+url+'" alt="'+alt+'" />');
+        $("<img/>")
+            .addClass("slide-img prev-img")
+            .attr("src", url)
+            .attr("alt", alt)
+            .prependTo(largeimgview);
+
          $(".prev-img").on("load", function() {
             target.animate({"right" : -500}, 100, function(){
-                target.removeClass("active-img");
-                target.addClass("next-img");
-                target.remove();
+                target.removeClass("active-img")
+                    .addClass("next-img")
+                    .remove();
             });            
-            var current = $(".prev-img");
-            var slide = (window.outerWidth-current.outerWidth())/2;
+            var current = $(".prev-img"),
+                slide = (window.outerWidth-current.outerWidth())/2;
+
             current.animate({"left" : slide}, 100, function(){
-                current.removeClass("prev-img");
-                current.css("left", "");
-                current.addClass("active-img");                
+                current.removeClass("prev-img")
+                    .css("left", "")
+                    .addClass("active-img");                
             });            
         }); 
     }
     $(".slide-img").on("load", function(){
-        var width = $(this).outerWidth();
-        var height = $(this).outerHeight();
-        $(this).css({"max-width":width+"px", "max-height":height+"px", "height":"100%"});
+            var width = $(this).outerWidth(),
+                height = $(this).outerHeight();
+
+        $(this).css({"max-width":width, "max-height":height, "height":"100%"});
     });
 }
 
 function checkURL() {
-    var url = document.location.href;
-    var upd = (url.indexOf("imgId=")>0) ? url.slice(url.indexOf("imgId=")+6) : "";
+    var url = document.location.href,
+        upd = (url.indexOf("imgId=")>0) ? url.slice(url.indexOf("imgId=")+6) : "";
+
     upd = (upd.indexOf("&")>0) ? upd.slice(0, upd.indexOf("&")) : upd;
 
     return upd;
 }
 
 function scrollgallery(active) {
-    var prev_elem = $(".list-photo-XXS").find(".active");
+    var listphotoXXS = $(".list-photo-XXS"),
+        listitems = listphotoXXS.find(".list-item"),
+        prev_elem = listphotoXXS.find(".active"),
+        prev_index = listitems.index(prev_elem),
+        index = listitems.index(active),
+        li_width = listitems.outerWidth()+parseInt(listitems.css("margin-left"))+parseInt(listitems.css("margin-right")),
+        ul_width = listitems.size() * li_width,
+        wrapper_width = listphotoXXS.parent().outerWidth(),
+        slide = Math.round((index+1)*li_width-wrapper_width/2-li_width/2);
+
     prev_elem.removeClass("active");
-    var prev_index = $(".list-photo-XXS .list-item").index(prev_elem);
-    $(active).addClass("active");
-    var index = $(".list-photo-XXS .list-item").index(active);
-    var li_width = $(".list-item").outerWidth()+parseInt($(".list-item").css("margin-left"))+parseInt($(".list-item").css("margin-right"));
-    var ul_width = $(".list-photo-XXS li").size() * li_width;
-    var wrapper_width = $(".list-photo-XXS").parent().outerWidth();
-    var slide = Math.round((index+1)*li_width-wrapper_width/2-li_width/2);
-    if(slide>0 && slide<ul_width-wrapper_width) $(".list-photo-XXS").animate({"left" : -slide}, 500);
-    else if(slide<ul_width-wrapper_width) $(".list-photo-XXS").animate({"left" : 0}, 500);
-    else $(".list-photo-XXS").animate({"left" : -(ul_width-wrapper_width)}, 500);
+     $(active).addClass("active");
+    if(slide>0 && slide<ul_width-wrapper_width) listphotoXXS.animate({"left" : -slide}, 500);
+    else if(slide<ul_width-wrapper_width) listphotoXXS.animate({"left" : 0}, 500);
+    else listphotoXXS.animate({"left" : -(ul_width-wrapper_width)}, 500);
 
     return (prev_index>=0) ? index-prev_index : 0;
 }
